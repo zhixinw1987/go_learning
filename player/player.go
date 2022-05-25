4c89b8efd92a46fde7b334e10676391d6d9557b7
zhixinw1987 <zhixinw1987@gmail.com>
2022-05-25 15:00:28 +0800

    add player module, define, initiate and assign struct

---
+++ player/player.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+type player struct {
+	firstName string
+	lastName string
+	age int
+	//contactInfo contactInfo
+	//embeded struct can use same attribute name as the type name, just simple remove the attribute name only declare the type
+	//the default attribute name will be applied same as the type name "contactInfo contactInfo" 
+	contactInfo
+}
+
+type contactInfo struct {
+	email string
+	postcode string
+}
+
+func main (){
+	//specify attribute name when assigning the initiate value
+	p1:=player{
+		age:35, 
+		lastName:"wang", 
+		firstName:"zhixin",
+		contactInfo: contactInfo {
+			//whenever using multi-line struct, every single line must end with "," even if it's the last line
+			email:"zhixinw1987@gmail.com",
+			postcode:"521601",
+		},
+	}
+	fmt.Println(p1.age)
+	fmt.Printf("%+v", p1)
+	//else all attribute must be assigned by sequence in which the attributes are declared
+	p2:=player{"yunqing", "wang", 65, contactInfo{"wangzx870320@gmail.com", "610081"}}
+	fmt.Println(p2)
+	//declare a player variable p3 with empty initiate value
+	var p3 player
+	fmt.Println(p3.age)
+}
